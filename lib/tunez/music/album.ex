0a0eca159bfb417879d35d6fccaf15df7acada9c
Roy Veshovda <roy@veshovda.no>
2025-09-03 11:25:10 +0000

    Implement album resource actions and validations; update artist relationship sorting

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -32,4 +32,38 @@ defmodule Tunez.Music.Album do
       allow_nil? false
     end
   end
+
+  identities do
+    identity :unique_album_names_per_artist, [:name, :artist_id],
+    message: "already exists for this artist"
+  end
+
+  actions do
+    defaults [:read, :destroy]
+
+    create :create do
+      accept [:name, :year_released, :cover_image_url, :artist_id]
+    end
+
+    update :update do
+      accept [:name, :year_released, :cover_image_url]
+    end
+  end
+
+  validations do
+    validate numericality(:year_released,
+                          greater_than: 1950,
+                          less_than_or_equal_to: &__MODULE__.next_year/0
+                  ),
+                  where: [present(:year_released)],
+                  message: "must be between 1950 and next year"
+
+    validate match(:cover_image_url,
+              ~r"^(https://|/images/).+(\.png|\.jpg)$"
+              ),
+              where: [changing(:cover_image_url)],
+              message: "must start with https:// or /images/"
+  end
+
+  def next_year, do: Date.utc_today().year + 1
 end
